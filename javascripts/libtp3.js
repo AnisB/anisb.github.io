78a283649a8617f94929aad68d01d9265e5980f5
Anis Benyoub <anisbenyoub@MacBook-Pro-de-Anis.local>
2015-09-27 17:25:22 +0200

    correction error chrome

--- javascripts/libtp3.js
+++ javascripts/libtp3.js
@@ -239,32 +239,29 @@ function injectVec3(parProgam, parVec4Name, parVec4)
     gl.uniform3fv(vecID, false, parVec4);
 }
 
-function CreateTexture() 
+function CreateTexture(parWidth, parHeight) 
 {
     var texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
- 
     // Set up texture so we can render any size image and so we are
     // working with pixels.
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
- 
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, parWidth, parHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
     return texture;
   }
 
 function CreateFrameBufferObject(parWidth, parHeight)
 {
    
-    var colorTex = CreateTexture();
-    gl.bindTexture(gl.TEXTURE_2D, colorTex);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, parWidth, parHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-
+    var colorTex = CreateTexture(parWidth, parHeight);
+   
     // // Create the depth texture
     var depthbuffer = gl.createRenderbuffer();
     gl.bindRenderbuffer(gl.RENDERBUFFER, depthbuffer);
